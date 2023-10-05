7088a997cb18a247e531246ff5a13177537afea3
holyhost <2024212728@qq.com>
2023-10-05 22:50:19 +0800

    feat: add error page

---
+++ app/error.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { Button, Container, Group, Title, Text } from '@mantine/core'
+import React from 'react'
+import classes from '@/styles/error.module.css'
+
+const RootError = () => {
+  return (
+    <div className={classes.root}>
+    <Container>
+      <div className={classes.label}>500</div>
+      <Title className={classes.title}>Something bad just happened...</Title>
+      <Text size="lg" ta="center" className={classes.description}>
+        Our servers could not handle your request. Don&apos;t worry, our development team was
+        already notified. Try refreshing the page.
+      </Text>
+      <Group justify="center">
+        <Button variant="white" size="md">
+          Refresh the page
+        </Button>
+      </Group>
+    </Container>
+  </div>
+  )
+}
+
+export default RootError
