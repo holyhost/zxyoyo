b5098246f776d1e38ac67bac9b3a381d352381cd
holyhost <2024212728@qq.com>
2023-10-06 23:02:08 +0800

    feat: add beian number

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Button, Group } from '@mantine/core';
+import { Meta } from '@/components/Meta';
+import BeianFooter from '@/components/layout/BeianFooter';
 
 export default function HomePage() {
   return (
     <>
+      <Meta title='只想优优' description='只想优优主页，中国古诗词！'/>
       <Welcome />
       <ColorSchemeToggle />
       <Group justify="center" mt="xl">
-        <Button component='a' href='/home' variant="filled">home</Button>
+        <Button component='a' href='/home' variant="filled">主页</Button>
       </Group>
+      <BeianFooter/>
     </>
   );
 }
---
+++ components/layout/BeianFooter.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BeianFooter = () => {
+  return (
+    <div style={{textAlign: 'center', position: 'absolute', bottom: 0, left: '50%', transform: 'translate(-50%, -50%)'}}>
+      <a href="https://beian.miit.gov.cn/" target="_blank" style={{color:'gray', textDecoration: 'none'}}>
+      湘ICP备20002140号-2
+      </a>
+    </div>
+  )
+}
+
+export default BeianFooter
