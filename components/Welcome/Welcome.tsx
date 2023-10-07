90bfe0f34302d80367f2d77c1e8820dbadff3715
holyhost <2024212728@qq.com>
2023-10-07 09:12:41 +0800

    feat: update welcome text

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,23 +1,26 @@
-import { Title, Text, Anchor } from '@mantine/core';
+import { Title, Text, Anchor, Divider } from '@mantine/core';
 import classes from './Welcome.module.css';
 
 export function Welcome() {
   return (
     <>
-      <Title className={classes.title} ta="center" mt={100}>
-        Welcome to{' '}
+      <Title className={classes.title} ta="center" pt={100}>
+        欢迎来到{' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+          只想优优
         </Text>
       </Title>
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Next.js project includes a minimal setup for server side rendering, if you want
-        to learn more on Mantine + Next.js integration follow{' '}
-        <Anchor href="https://mantine.dev/guides/next/" size="lg">
-          this guide
-        </Anchor>
-        . To get started edit page.tsx file.
+        ---我们来来回回反反复复，最终还是为了找回小时候一个人哪怕只是玩玩泥巴还能够的那份沉静和认真劲
       </Text>
+      
+      <Anchor href='/home'>
+        <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+          中华文化五千年，煌煌巨著随处见，春秋战国有孔孟，百花争鸣巨人间，汉赋唐诗广流传，宋词元曲非等闲，明清小说人知晓，现代文艺领风骚!
+        </Text>
+      </Anchor>
+      <Divider label={'----'} />
+
     </>
   );
 }
