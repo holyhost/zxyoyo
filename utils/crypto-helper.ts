e9d64932cf016576e694cfa523490a486a6f0575
holyhost <2024212728@qq.com>
2023-10-11 22:37:21 +0800

    feat: add cryto util

---
+++ utils/crypto-helper.ts
@@ -0,0 +1,17 @@
+import * as CryptoJS from 'crypto-js';
+import { AES } from 'crypto-js';
+
+
+export function toAesSource(mess: string, secretKey = 'zxyoyo.com') {
+
+    return AES.decrypt(mess, enhenceKey(secretKey)).toString(CryptoJS.enc.Utf8);
+}
+
+export function toAesString(mess: string, secretKey = 'zxyoyo.com') {
+
+    return AES.encrypt(mess, enhenceKey(secretKey)).toString();
+}
+
+export const mymd5 = (key: string) => CryptoJS.MD5('my' + key + (key.toString().length * 2) + key + 'key')
+export const md5 = (s: string) => CryptoJS.MD5(s)
+const enhenceKey = (key: string) => CryptoJS.MD5('zxyo' + key + 'yo' + key.toString().length + '.com')
