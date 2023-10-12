4069610e141482bd95bdc1f6820ad8bc0c24ae7a
holyhost <2024212728@qq.com>
2023-10-12 22:31:37 +0800

    feat: add api for delete post by post id

---
+++ app/api/posts/delete/route.ts
@@ -0,0 +1,32 @@
+import { connectToDB } from '@/utils/database'
+import { NextRequest, NextResponse } from 'next/server'
+import { getServerSession } from "next-auth/next"
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import Post from '@/models/post'
+
+export async function GET(request: NextRequest) {
+    console.log('come into post delete')
+    const { searchParams } = new URL(request.url)
+    const id = searchParams.get('id')
+    console.log('id', id)
+    if( !id || id.length < 10){
+        return NextResponse.json({ success: false }, {status: 401})
+    }
+    const session = await getServerSession(authOptions)
+    if (session && session.user && session.user._id) {
+        const uid = session.user._id.toString()
+        await connectToDB()
+        // user can only delete self file
+        await Post.findOneAndDelete({
+            uid: uid,
+            name: id
+        })
+        
+        
+        return NextResponse.json({ success: true})
+    }else{
+        return NextResponse.json({ success: false, data: 'login pls' }, {status: 401})
+    }
+
+    
+}
