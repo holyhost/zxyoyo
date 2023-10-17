2ba6b50d47e5e2ccdae336e318e6595d76fc6be2
holyhost <2024212728@qq.com>
2023-10-17 21:35:55 +0800

    try to update session but failed

--- components/provider/UserProvider.tsx
+++ components/provider/UserProvider.tsx
@@ -2,6 +2,21 @@
 import { SessionProvider } from 'next-auth/react'
 import React, { ReactNode } from 'react'
 
+// type UserSession = {
+//   email?: string
+//   image?: string, 
+//   keys?: string, 
+//   name?: string, 
+//   password?: string, 
+//   role?: string, 
+//   _id?: string, 
+// }
+
+// type MySession = {
+//   expires: string,
+//   user: UserSession
+// }
+
 const UserProvider = ({children, session}:{children: ReactNode, session: any}) => {
   return (
     <SessionProvider session={session}>
