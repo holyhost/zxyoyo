65780092dbfc2ce50136d4731d508addcd1432a1
holyhost <2024212728@qq.com>
2023-10-28 16:56:53 +0800

    update: remove useless code

--- app/posts/new/page.tsx
+++ app/posts/new/page.tsx
@@ -3,20 +3,10 @@ import { Container } from '@mantine/core';
 import { AppLayout } from '@/components/layout/AppLayout';
 import SimplePostForm from '@/components/POSTS/SimplePostForm';
 
-const getUserDetail = async ()=> {
 
-  const res = await fetch( process.env.NEXT_PUBLIC_APP_HOST + '/api/user')
-  if (!res.ok) {
-    return null
-  }
- 
-  return res.json()
-
-}
 
 
 const NewPost = async() => {
-  const data = await getUserDetail()
   
   return (
     <AppLayout login={true}>
