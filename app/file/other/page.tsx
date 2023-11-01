fddf6393af48f976b9f7a800c917c441bea2fdc4
holyhost <2024212728@qq.com>
2023-11-01 23:10:23 +0800

    feat: add page query and ending

--- app/file/other/page.tsx
+++ app/file/other/page.tsx
@@ -1,33 +1,45 @@
 "use client"
 import React, { useEffect, useState } from 'react'
-import { Container, Stack } from '@mantine/core';
+import { Center, Container, Loader, Stack } from '@mantine/core';
 import { AppLayout } from '@/components/layout/AppLayout';
 import { notifications } from '@mantine/notifications';
 import OtherImageCard from '@/components/MyImage/OtherImageCard';
 import { Masonry } from 'masonic';
 import { useCurrentUser } from '@/store/user.store';
+import { useInView } from 'react-intersection-observer';
+import { EndOfFeed } from '@/components/EndOfFeed/EndOfFeed';
 
 const OtherPage = () => {
-  const [files, setFiles] =  useState<any[]>([])
+  const [files, setFiles] = useState<any[]>([])
+  const [pageIndex, setPageIndex] = useState<number>(0)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [end, setEnd] = useState<boolean>(false)
   useCurrentUser()
-  useEffect(()=>{
-    const getData =  async()=>{
-      const res = await fetch('/api/file/other')
+  const { ref, inView } = useInView();
+  console.log(inView)
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true)
+      const res = await fetch('/api/file/other?page='+pageIndex)
       const j = await res.json()
-      console.log(j)
-      if(j.success) setFiles(j.data)
+      if (j.success) {
+        setFiles([...files,...j.data])
+        setPageIndex(pageIndex + 1)
+        if(j.data.length < 20) setEnd(true)
+      }
+      setIsLoading(false)
     }
-    getData()
-  },[])
-  const deleteFile =async ( file:string) => {
+    if(inView && !isLoading && !end) getData()
+  }, [inView, isLoading])
+  const deleteFile = async (file: string) => {
     const res = await fetch('/api/file/delete?id=' + file)
-    if(res.ok){
+    if (res.ok) {
       notifications.show({
         title: "删除成功",
         message: `${file} 已删除！`
       })
-      setFiles(files.filter(name=> name !== file))
-    }else{
+      setFiles(files.filter(name => name !== file))
+    } else {
       notifications.show({
         title: "删除失败",
         message: `校验失败，文件未删除！`
@@ -38,10 +50,14 @@ const OtherPage = () => {
   return (
     <AppLayout>
       <Container>
-      {files.length>0 && <Masonry items={files} 
-                          render={OtherImageCard} 
-                          columnGutter={28} 
-                          itemKey={item => item.id}/>}
+        {files.length > 0 && <Masonry items={files}
+          render={OtherImageCard}
+          columnGutter={28}
+          itemKey={item => item.id} />}
+        <Center ref={ref} mt="md">
+          {!end && inView && <Loader />}
+        </Center>
+        {end && <EndOfFeed/>}
       </Container>
     </AppLayout>
 
