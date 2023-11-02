1d37ac1f02c77ea495d73ca0fb477f77402b624c
holyhost <2024212728@qq.com>
2023-11-02 15:50:31 +0800

    udpate

--- app/file/other/page.tsx
+++ app/file/other/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useEffect, useState } from 'react'
-import { Center, Container, Loader, Stack } from '@mantine/core';
+import { Center, Container, Loader, Flex, Stack } from '@mantine/core';
 import { AppLayout } from '@/components/layout/AppLayout';
 import { notifications } from '@mantine/notifications';
 import OtherImageCard from '@/components/MyImage/OtherImageCard';
@@ -14,22 +14,75 @@ const OtherPage = () => {
   const [pageIndex, setPageIndex] = useState<number>(0)
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [end, setEnd] = useState<boolean>(false)
-  useCurrentUser()
+  const backendHost = process.env.NEXT_PUBLIC_BACKEND_HOST
+  const imgHost = backendHost + "/app"
+  const curUser = useCurrentUser()
   const { ref, inView } = useInView();
   console.log(inView)
+
+  const deleteImage = async(data: any,onlysucai?: boolean, onlyfile?:boolean)=>{
+    const fd = new FormData()
+    fd.set('keynames', curUser?.keynames ?? '')
+    fd.set('ids', data.id + '')
+    if(onlysucai){
+      fd.set('sucai', 'onlysucai')
+    }
+    if(onlyfile){
+      fd.set('file', 'onlyfile')
+    }
+    const result = await fetch(backendHost + "/filemng/api/file/delete",{
+      method: 'POST',
+      body: fd
+    })
+    const dd = await result.json()
+    console.log(dd)
+    if(dd.status === 'ok'){
+      if(onlysucai){
+        const file = files.find(item => item.id === data.id)
+        if(file) {
+          file.mark = ''
+          setFiles([...files])
+        }
+      }else{
+        const newFiles = files.filter(item => item.id !== data.id)
+        setFiles([...newFiles])
+      }
+      
+    }
+  }
+  
+  const uploadSucai = async (id: number) => {
+    console.log('click upload sucai')
+    const fd = new FormData()
+    fd.set('keynames', curUser?.keynames ?? '')
+    fd.set('id', id + '')
+    const result = await fetch("/api/file/other",{
+      method: 'POST',
+      body: fd
+    })
+    const dd = await result.json()
+    if(dd.success) {
+      const file = files.find(item => item.id === id)
+      if(file) {
+        file.mark = dd.data
+        setFiles([...files])
+      }
+    }
+  }
   useEffect(() => {
     const getData = async () => {
       setIsLoading(true)
-      const res = await fetch('/api/file/other?page='+pageIndex)
+      const res = await fetch('/api/file/other?page=' + pageIndex)
       const j = await res.json()
       if (j.success) {
-        setFiles([...files,...j.data])
+        const tempArr = j.data.map((item: any) => ({...item, 'spath': imgHost + item.spath, 'bpath': imgHost + item.bpath}))
+        setFiles([...files, ...tempArr])
         setPageIndex(pageIndex + 1)
-        if(j.data.length < 20) setEnd(true)
+        if (j.data.length < 20) setEnd(true)
       }
       setIsLoading(false)
     }
-    if(inView && !isLoading && !end) getData()
+    if (inView && !isLoading && !end) getData()
   }, [inView, isLoading])
   const deleteFile = async (file: string) => {
     const res = await fetch('/api/file/delete?id=' + file)
@@ -49,16 +102,26 @@ const OtherPage = () => {
 
   return (
     <AppLayout>
-      <Container>
-        {files.length > 0 && <Masonry items={files}
+
+        {files.length > 0 && <Flex gap="md"
+          justify="center"
+          align="center"
+          direction="row"
+          wrap="wrap">
+          {files.map(item => <OtherImageCard 
+                          key={item.id} 
+                          data={item}
+                          upload={uploadSucai} 
+                          onDelete={deleteImage} />)}
+        </Flex>}
+        {/* {files.length > 0 && <Masonry items={files}
           render={OtherImageCard}
           columnGutter={28}
-          itemKey={item => item.id} />}
+          itemKey={item => item.id} />} */}
         <Center ref={ref} mt="md">
           {!end && inView && <Loader />}
         </Center>
-        {end && <EndOfFeed/>}
-      </Container>
+        {end && <EndOfFeed />}
     </AppLayout>
 
   )
--- components/MyImage/OtherImageCard.tsx
+++ components/MyImage/OtherImageCard.tsx
@@ -1,71 +1,33 @@
-"use client"
 import { ActionIcon, Badge, Card, Group, Text, Image, Menu, rem } from "@mantine/core"
 import classes from './OtherImageCard.module.css';
 import { OtherImageBean } from "@/bean/OtherImageBean";
 import { IconTrash, IconDotsVertical, IconDownload, IconClock, IconFileUpload, IconBrandWechat } from "@tabler/icons-react";
-import { useEffect, useState } from "react";
-import { useCurrentUser } from "@/store/user.store";
+import Link from "next/link";
 
-const OtherImageCard = ({ index, data, width }: Props) => {
+const OtherImageCard = ({ index, data, width, upload,update, onDelete}: Props) => {
   // const isSucai = data.mark.includes('xx,xx')
-  const [deleted, setDeleted] = useState(false)
-  const [hasSucai, setSucai] = useState(data.mark ? true : false)
-  const backendHost = process.env.NEXT_PUBLIC_BACKEND_HOST
-  const imgHost = backendHost + "/app"
-  const curUser = useCurrentUser()
+  // const [deleted, setDeleted] = useState(false)
   const deleteImage = async(onlysucai?: boolean, onlyfile?:boolean)=>{
-    const fd = new FormData()
-    fd.set('keynames', curUser?.keynames ?? '')
-    fd.set('ids', data.id + '')
-    if(onlysucai){
-      fd.set('sucai', 'onlysucai')
-    }
-    if(onlyfile){
-      fd.set('file', 'onlyfile')
-    }
-    const result = await fetch(backendHost + "/filemng/api/file/delete",{
-      method: 'POST',
-      body: fd
-    })
-    const dd = await result.json()
-    console.log(dd)
-    if(dd.status === 'ok'){
-      if(onlysucai){
-        setSucai(false)
-      }else{
-        setDeleted(true)
-      }
-      
-    }
+    onDelete && onDelete(data, onlysucai, onlyfile)
   }
   const uploadSucai = async () => {
-    console.log('click upload sucai')
-    const fd = new FormData()
-    fd.set('keynames', curUser?.keynames ?? '')
-    fd.set('id', data.id + '')
-    const result = await fetch("/api/file/other",{
-      method: 'POST',
-      body: fd
-    })
-    const dd = await result.json()
-    if(dd.success) setSucai(true)
+    upload && upload(data.id)
   }
 
-  useEffect(()=>{
-    
-  },[])
   return (
 
     <Card withBorder padding="lg" mt="xs" radius="md" maw="20rem" miw="10rem">
 
-      <Card.Section className={deleted ? classes.filterGray : ''}>
-        <Image src={imgHost + data.spath}></Image>
+      <Card.Section>
+        <Image src={data.spath ? (data.spath):'/noimg.png'}></Image>
         <Group className={classes.floatingMenu} justify="flex-end" mt="0" mb="0">
           {/* <Text fw={500}>{data.type}</Text> */}
           {data.type && <Badge color="pink" variant="light">
             {data.type}
           </Badge>}
-          {hasSucai && <IconBrandWechat color="green" size={'16'}/>}
+          {data.mark && <Link href={data.mark.split('xx,xx')[1]} target="_blank">
+            <IconBrandWechat color="green" size={'16'}/>  
+          </Link>}
           <Menu trigger="hover" withinPortal position="bottom-end" shadow="sm">
             <Menu.Target>
               <ActionIcon variant="subtle" color="gray">
@@ -76,19 +38,19 @@ const OtherImageCard = ({ index, data, width }: Props) => {
             <Menu.Dropdown>
               <Menu.Item
                 component="a" 
-                href={imgHost +data.bpath} 
+                href={data.bpath} 
                 rel="noopener noreferrer" 
                 download 
                 leftSection={<IconDownload style={{ width: rem(14), height: rem(14) }} />}>
                 下载原图
               </Menu.Item>
-              {!hasSucai&& <Menu.Item onClick={uploadSucai} leftSection={<IconFileUpload style={{ width: rem(14), height: rem(14) }} />}>
+              {!data.mark&& <Menu.Item onClick={uploadSucai} leftSection={<IconFileUpload style={{ width: rem(14), height: rem(14) }} />}>
                 上传素材
               </Menu.Item>}
               <Menu.Item leftSection={<IconClock style={{ width: rem(14), height: rem(14) }} />}>
                 {data.createtime}
               </Menu.Item>
-              {hasSucai && <Menu.Item
+              {data.mark && <Menu.Item
                 leftSection={<IconTrash style={{ width: rem(14), height: rem(14) }} />}
                 color="red"
                 onClick={()=> deleteImage(true)}
@@ -113,7 +75,7 @@ const OtherImageCard = ({ index, data, width }: Props) => {
           </Menu>
         </Group>
       </Card.Section>
-      {deleted && <IconTrash className={classes.floatingCenter} color="teal" size={'40px'}/>}
+      {/* {deleted && <IconTrash className={classes.floatingCenter} color="teal" size={'40px'}/>} */}
 
     </Card>
   )
@@ -122,7 +84,10 @@ const OtherImageCard = ({ index, data, width }: Props) => {
 export default OtherImageCard
 
 type Props = {
-  index: number,
+  index?: number,
   data: OtherImageBean,
-  width: number
+  width?: number,
+  upload?: (id: number)=> void,
+  update?: ()=> void,
+  onDelete?: (data: OtherImageBean,onlysucai?: boolean, onlyfile?:boolean)=> void
 }
