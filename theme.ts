8f257b01b0a8e680b77213d614a40704079ab431
holyhost <2024212728@qq.com>
2023-11-09 22:15:10 +0800

    update theme color

--- theme.ts
+++ theme.ts
@@ -1,7 +1,24 @@
 'use client';
 
-import { createTheme } from '@mantine/core';
+import { MantineColorsTuple, createTheme } from '@mantine/core';
 
+
+const myColor: MantineColorsTuple = [
+  "#effbf7",
+  "#dff3ed",
+  "#bae9da",
+  "#91ddc5",
+  "#71d4b4",
+  "#5dcea9",
+  "#51cba3",
+  "#42b38f",
+  "#369f7e",
+  "#238a6b"
+]
 export const theme = createTheme({
   /* Put your mantine theme override here */
+  primaryColor: 'myColor',
+  colors: {
+    myColor
+  }
 });
