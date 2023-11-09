ed352f3cec1b135f4d79bbf6be6d4e4a7805bc75
holyhost <2024212728@qq.com>
2023-11-09 21:41:06 +0800

    feat: add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,17 @@
+import PoemList from '@/components/gushi/PoemList'
+import { AppLayout } from '@/components/layout/AppLayout'
+import { Container } from '@mantine/core'
+import React from 'react'
+
+
+const AboutPage = async() => {
+  return (
+    <AppLayout>
+        <Container mt={'lg'}>
+            this. about page...
+        </Container>
+    </AppLayout>
+  )
+}
+
+export default AboutPage
