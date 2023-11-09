9de64005246e3db9eee462aa83a35ea833826e0d
holyhost <2024212728@qq.com>
2023-11-09 17:23:39 +0800

    update header

---
+++ app/pricing/page.tsx
@@ -0,0 +1,17 @@
+import PoemList from '@/components/gushi/PoemList'
+import { AppLayout } from '@/components/layout/AppLayout'
+import { Container } from '@mantine/core'
+import React from 'react'
+
+
+const Home = async() => {
+  return (
+    <AppLayout>
+        <Container mt={'lg'}>
+            this. pricing page...
+        </Container>
+    </AppLayout>
+  )
+}
+
+export default Home
--- components/layout/AppHeader.tsx
+++ components/layout/AppHeader.tsx
@@ -9,16 +9,16 @@ import classes from './HeaderMenu.module.css';
 // import { MantineLogo } from '@mantine/ds';
 const links = [
   { link: '/posts', label: 'Post' },
-  {
-    link: '#1',
-    label: 'Learn',
-    links: [
-      { link: '/docs', label: 'Documentation' },
-      { link: '/resources', label: 'Resources' },
-      { link: '/community', label: 'Community' },
-      { link: '/blog', label: 'Blog' },
-    ],
-  },
+  // {
+  //   link: '#1',
+  //   label: 'Learn',
+  //   links: [
+  //     { link: '/docs', label: 'Documentation' },
+  //     { link: '/resources', label: 'Resources' },
+  //     { link: '/community', label: 'Community' },
+  //     { link: '/blog', label: 'Blog' },
+  //   ],
+  // },
   { link: '/about', label: 'About' },
   { link: '/pricing', label: 'Pricing' },
   {
