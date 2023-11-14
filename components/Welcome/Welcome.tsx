304bf6b98f19b26bb6304fb2c3a1b7120594dcf7
holyhost <2024212728@qq.com>
2023-11-14 14:35:03 +0800

    update width

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -10,12 +10,12 @@ export function Welcome() {
           只想优优
         </Text>
       </Title>
-      <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+      <Text px={'lg'} c="dimmed" ta="center" size="lg" maw={520} mx="auto" mt="xl">
         ---我们来来回回反反复复，最终还是为了找回小时候一个人哪怕只是玩玩泥巴还能够的那份沉静和认真劲
       </Text>
 
       <Anchor href='/home'>
-        <Text variant="gradient" ta="center" size="lg" maw={580} mx="auto" mt="xl" gradient={{ from: 'red', to: 'indigo', deg: 156 }}>
+        <Text px={'lg'} variant="gradient" ta="center" size="lg" maw={560} mx="auto" mt="xl" gradient={{ from: 'red', to: 'indigo', deg: 156 }}>
           中华文化五千年，煌煌巨著随处见，春秋战国有孔孟，百花争鸣巨人间，汉赋唐诗广流传，宋词元曲非等闲，明清小说人知晓，现代文艺领风骚!
         </Text>
       </Anchor>
