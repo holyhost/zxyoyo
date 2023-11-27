1726cd9e4b71ab14cd3140c36fe8df87856a64ef
holyhost <2024212728@qq.com>
2023-11-27 22:56:31 +0800

    feat: add meta and refresh timeout

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,15 @@
 import PoemList from '@/components/gushi/PoemList'
 import { AppLayout } from '@/components/layout/AppLayout'
 import { Container } from '@mantine/core'
+import { Metadata } from 'next'
 import React from 'react'
+
+
+export const metadata: Metadata = {
+  title: '每日古诗词分享',
+  description: '一起来学古诗词，每日古诗词分享，李白、杜甫、苏轼...'
+}
+export const revalidate = 3600 * 24 // revalidate at most every 2 hour
 
 const getData = async () =>{
     const res = await fetch( process.env.NEXT_PUBLIC_APP_HOST + '/api/gsc')
