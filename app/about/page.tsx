925316a330f20e9ea904a02b9adfba9ca4a48cb0
holyhost <2024212728@qq.com>
2023-12-10 11:12:50 +0800

    feat: add meta info

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,14 @@
 import UpdateHistory from '@/components/UpdateHistory/UpdateHistory'
 import { AppLayout } from '@/components/layout/AppLayout'
 import { Code, Container, Divider, Text } from '@mantine/core'
+import { Metadata } from 'next'
 import React from 'react'
 
+
+export const metadata: Metadata = {
+  title: '关于-只想优优',
+  description: '关于只想优优的介绍！关于只想优优的更新记录，关于只想优优的详细介绍'
+}
 
 const AboutPage = async() => {
   return (
