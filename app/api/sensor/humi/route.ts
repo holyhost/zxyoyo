94bc1756b3b092be61e8d0f3598931812b80bfb3
holyhost <2024212728@qq.com>
2024-03-18 21:50:20 +0800

    add get sensor data

--- app/api/sensor/humi/route.ts
+++ app/api/sensor/humi/route.ts
@@ -4,7 +4,6 @@ import SensorData from '@/models/sensor-data'
 
 
 export const GET = async(request: NextRequest)=> {
-    console.log('posts come into get')
     const { searchParams } = new URL(request.url)
     const token = searchParams.get('token')
     const maker = searchParams.get('maker')
@@ -20,4 +19,20 @@ export const GET = async(request: NextRequest)=> {
         createTime: new Date().getTime().toString()
     })
     return NextResponse.json({ success: true })
+}
+
+export const POST = async(request: NextRequest)=> {
+    const { searchParams } = new URL(request.url)
+    const token = searchParams.get('token')
+    const stime = searchParams.get('stime')
+    const etime = searchParams.get('etime')
+    const name = searchParams.get('name')
+    if(token != 'K123qwelolqwel--utnAB') return NextResponse.json({ success: false, data: 'login pls' }, {status: 401})
+    await connectToDB()
+    const data = await SensorData.find({name, createTime: { $elemMatch: { $gte: stime, $lte: etime}}})
+    if( data){
+        return NextResponse.json({ success: true, data })
+    }        
+    
+    return NextResponse.json({ success: false },{ status: 501})
 }
