1c42291d5d439510673f78e0ea0ca3feb7b93cb2
holyhost <2024212728@qq.com>
2024-04-15 16:07:41 +0800

    add wangan bei an

--- components/layout/BeianFooter.tsx
+++ components/layout/BeianFooter.tsx
@@ -6,6 +6,8 @@ const BeianFooter = () => {
       <a href="https://beian.miit.gov.cn/" target="_blank" style={{color:'gray', textDecoration: 'none'}}>
       湘ICP备20002140号-2
       </a>
+      <img style={{verticalAlign: 'middle', marginLeft: '1rem', height: '1rem', width: '1rem'}} src='/icon-wa.png' />
+      <a href="https://beian.mps.gov.cn/#/query/webSearch?code=43112402000142" style={{color:'gray', textDecoration: 'none'}} rel="noreferrer" target="_blank">湘公网安备43112402000142</a>
     </div>
   )
 }
